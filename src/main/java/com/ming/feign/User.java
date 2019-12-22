0b32a7239526bd9fc2eeb7b60b6e4ccc5e0e1756
minglie <934031452@qq.com>
2019-12-22 22:49:17 +0800

    修改版本

--- src/main/java/com/ming/feign/User.java
+++ src/main/java/com/ming/feign/User.java
@@ -1,16 +1,10 @@
 package com.ming.feign;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-/**
- * 类名可以随意, 字段需要保持一致
- * 
- * @author 晓风轻
- *
- */
+import lombok.Data;
+
+
+
 @Data
 public class User {
 
