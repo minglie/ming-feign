8c03ed8ed33527d86bc9e3936c40c70234cfa443
minglie <934031452@qq.com>
2020-06-06 16:11:56 +0800

    add exmple

--- ming-feign-sample/src/main/java/com/minglie/demo/MingFeignDemoApplication.java
+++ ming-feign-sample/src/main/java/com/minglie/demo/MingFeignDemoApplication.java
@@ -1,1 +1,13 @@
-package com.minglie.demo;import org.springframework.boot.SpringApplication;import org.springframework.boot.autoconfigure.SpringBootApplication;@SpringBootApplicationpublic class MingFeignDemoApplication {    public static void main(String[] args) {        SpringApplication.run(MingFeignDemoApplication.class, args);    }}
+package com.minglie.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MingFeignDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MingFeignDemoApplication.class, args);
+    }
+
+}
